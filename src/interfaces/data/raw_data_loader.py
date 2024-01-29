d5b875db8224c050826afdbcb6d0b9c7445b4723
pritchardn <21726929@student.uwa.edu.au>
2024-01-29 13:42:51 +0800

    Adds limit to raw_data loader based on percentage.

--- src/interfaces/data/raw_data_loader.py
+++ src/interfaces/data/raw_data_loader.py
@@ -3,27 +3,40 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 
+def _calc_limit_int(limit: float, data_len: int) -> int:
+    if limit is None:
+        return data_len
+    else:
+        return int(data_len * limit)
+
+
 class RawDataLoader(ABC):
-    def __init__(self, data_dir: str):
+
+    def __init__(self, data_dir: str, limit: float = None):
         self.train_x = None
         self.train_y = None
         self.test_x = None
         self.test_y = None
         self.rfi_models = []
         self.data_dir = data_dir
+        self.limit = limit
 
     @abstractmethod
     def load_data(self, excluded_rfi=None):
         pass
 
     def fetch_train_x(self) -> np.ndarray:
-        return self.train_x
+        limit = _calc_limit_int(self.limit, self.train_x.shape[0])
+        return self.train_x[:limit]
 
     def fetch_train_y(self) -> np.ndarray:
-        return self.train_y
+        limit = _calc_limit_int(self.limit, self.train_y.shape[0])
+        return self.train_y[:limit]
 
     def fetch_test_x(self) -> np.ndarray:
-        return self.test_x
+        limit = _calc_limit_int(self.limit, self.test_x.shape[0])
+        return self.test_x[:limit]
 
     def fetch_test_y(self) -> np.ndarray:
-        return self.test_y
+        limit = _calc_limit_int(self.limit, self.test_y.shape[0])
+        return self.test_y[:limit]
