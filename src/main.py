671f4b36c8e0494ade9c718cf7b145dda82c9ff8
pritchardn <21726929@student.uwa.edu.au>
2024-01-29 13:44:10 +0800

    Updates main.py to include minimal example.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,26 @@
+import pytorch_lightning as pl
+
 from data.data_loaders import HeraDataLoader
 from data.data_module_builder import DataModuleBuilder
 from data.spike_encoders import LatencySpikeEncoder
+from models.fc_latency import LitFcLatency
 
 
 def main():
+    EXPOSURE = 32
+    TAU = 1.0
+    BETA = 0.95
     data_builder = DataModuleBuilder()
-    data_source = HeraDataLoader("./data")
+    data_source = HeraDataLoader("./data", limit=0.1)
     data_builder.set_dataset(data_source)
-    spike_converter = LatencySpikeEncoder(exposure=32, tau=1.0, normalize=True)
+    spike_converter = LatencySpikeEncoder(exposure=EXPOSURE, tau=TAU, normalize=True)
     data_builder.set_encoding(spike_converter)
     data_module = data_builder.build(32)
+    print("Built data module")
+    model = LitFcLatency(512, 2048, 512, EXPOSURE, BETA)
+    print("Built model")
+    trainer = pl.trainer.Trainer()
+    trainer.fit(model, data_module)
 
 
 if __name__ == "__main__":
