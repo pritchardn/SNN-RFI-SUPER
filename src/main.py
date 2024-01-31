8ca7af219377a525adaf2395bcaf6ca312da404d
pritchardn <21726929@student.uwa.edu.au>
2024-01-31 17:25:54 +0800

    Updates main to show single inference after training.

--- src/main.py
+++ src/main.py
@@ -19,8 +19,14 @@ def main():
     print("Built data module")
     model = LitFcLatency(32, 128, 32, BETA)
     print("Built model")
-    trainer = pl.trainer.Trainer()
+    trainer = pl.trainer.Trainer(max_epochs=1)
     trainer.fit(model, data_module)
+    trainer.test(model, data_module)
+    model.eval()
+    x, y = next(iter(data_module.test_dataloader()))
+    spike_hat, mem_hat = model(x)
+    mask = spike_converter.decode_inference(spike_hat)
+    print(mask.shape)
 
 
 if __name__ == "__main__":
