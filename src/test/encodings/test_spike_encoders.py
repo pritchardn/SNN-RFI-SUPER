ed73a8078268218cef3477472de6c2cb16a5c231
pritchardn <21726929@student.uwa.edu.au>
2024-01-31 17:25:31 +0800

    Adds latency encoder decoder test.

---
+++ src/test/encodings/test_spike_encoders.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+
+import numpy as np
+
+from data.data_loaders import HeraDataLoader
+from data.data_module_builder import DataModuleBuilder
+from data.spike_encoders import LatencySpikeEncoder
+from interfaces.data.raw_data_loader import RawDataLoader
+
+
+def _fetch_example_dataset():
+    data_builder = DataModuleBuilder()
+    data_source = HeraDataLoader("./data", limit=0.1, patch_size=32, stride=32)
+    data_builder.set_dataset(data_source)
+    return data_source, data_builder
+
+
+def _test_decode_inference(data_source: RawDataLoader, data_builder: DataModuleBuilder,
+                           exposure: int):
+    converter = LatencySpikeEncoder(exposure=exposure, tau=1.0, normalize=True)
+    data_builder.set_encoding(converter)
+    data_builder.build(32)
+    mask = data_source.test_y
+    z = converter.decode_inference(converter.encode_y(mask))
+    return np.equal(mask, z).all()
+
+
+class TestLatencySpikeEncoder(TestCase):
+
+    def test_decode_inference_1(self):
+        data_source, data_builder = _fetch_example_dataset()
+        self.assertFalse(_test_decode_inference(data_source, data_builder, 1))
+
+    def test_decode_inference_2(self):
+        data_source, data_builder = _fetch_example_dataset()
+        self.assertTrue(_test_decode_inference(data_source, data_builder, 2))
+
+    def test_decode_inference_4(self):
+        data_source, data_builder = _fetch_example_dataset()
+        self.assertTrue(_test_decode_inference(data_source, data_builder, 4))
+
+    def test_decode_inference_16(self):
+        data_source, data_builder = _fetch_example_dataset()
+        self.assertTrue(_test_decode_inference(data_source, data_builder, 16))
