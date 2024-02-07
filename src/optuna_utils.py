8e55aa9610941c47f9fb3fcab14bd3ba53d506ce
pritchardn <21726929@student.uwa.edu.au>
2024-02-07 09:16:41 +0800

    Improves optuna result writing.

--- src/optuna_utils.py
+++ src/optuna_utils.py
@@ -1,7 +1,15 @@
-import os
 import json
+import os
+
 import optuna
 from optuna.trial import TrialState
+
+
+def prepare_trial_json(trial, metric_names):
+    out_vals = {}
+    for name, val in zip(metric_names, trial.values):
+        out_vals[name] = val
+    return {"params": trial.params, "values": out_vals, "id": trial.number}
 
 
 def main(optuna_db):
@@ -9,33 +17,28 @@ def main(optuna_db):
         storage = optuna.storages.RDBStorage(
             url=optuna_db,
         )
+        study_name = os.getenv("STUDY_NAME")
+        metric_names = ["accuracy", "mse", "auroc", "auprc", "f1"]
         print(f"Optuna DB: {optuna_db}")
-        study = optuna.load_study(study_name=os.getenv("STUDY_NAME"), storage=storage)
+        study = optuna.load_study(study_name=study_name, storage=storage)
         complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
         best_trials = study.best_trials
         print("Study statistics: ")
         print("  Number of finished trials: ", len(study.trials))
         print("  Number of complete trials: ", len(complete_trials))
         for trial in best_trials:
-            print("  Best trial: ")
-            print("    Value: ", trial.values)
-            print("    Params: ")
-            for key, value in trial.params.items():
-                print(f"      {key}: {value}")
-        with open("trials.json", "w") as ofile:
+            print(json.dumps(prepare_trial_json(trial, metric_names), indent=4))
+        with open(f"{study_name}_trials.json", "w") as ofile:
             completed_trials_out = []
             for trial_params in complete_trials:
-                completed_trials_out.append(trial_params.params)
+                completed_trials_out.append(
+                    prepare_trial_json(trial_params, metric_names)
+                )
             json.dump(completed_trials_out, ofile, indent=4)
-        with open("best_trial.json", "w") as ofile:
+        with open(f"{study_name}_best_trial.json", "w") as ofile:
             best_trials_out = []
             for trial_params in best_trials:
-                best_trials_out.append(
-                    {
-                        "params": trial_params.params,
-                        "values": trial_params.values,
-                    }
-                )
+                best_trials_out.append(prepare_trial_json(trial_params, metric_names))
             json.dump(best_trials_out, ofile, indent=4)
     else:
         raise ValueError("No optuna DB specified.")
