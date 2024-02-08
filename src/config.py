21e20b7effd3a113958a1087fd7c2d6c003fc36f
pritchardn <21726929@student.uwa.edu.au>
2024-02-08 11:00:39 +0800

    Updates default configs for hera.

--- src/config.py
+++ src/config.py
@@ -1,27 +1,27 @@
 DEFAULT_CONFIG = {
     "data_source": {
         "data_path": "./data",
-        "limit": 0.1,
+        "limit": 1.0,
         "patch_size": 32,
         "stride": 32,
         "dataset": "HERA",
     },
     "dataset": {
-        "batch_size": 32,
+        "batch_size": 18,
     },
     "model": {
         "type": "FC_LATENCY",
         "num_inputs": 32,
         "num_hidden": 128,
         "num_outputs": 32,
-        "beta": 0.95,
+        "beta": 0.8976593051095902,
     },
     "trainer": {
-        "epochs": 50,
+        "epochs": 60,
     },
     "encoder": {
         "method": "LATENCY",
-        "exposure": 8,
+        "exposure": 25,
         "tau": 1.0,
         "normalize": True,
     },
