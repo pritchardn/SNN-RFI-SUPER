528e5d4fbe05ccd7141f595b51d1398c85bd929c
pritchardn <21726929@student.uwa.edu.au>
2024-02-08 12:46:11 +0800

    Gets root dir from CLA

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+import os
 from config import DEFAULT_CONFIG
 from experiment import Experiment
 
 
 def main():
     config = DEFAULT_CONFIG
-    experiment = Experiment()
+    config["data_source"]["data_path"] = os.getenv("DATA_PATH", config["data_source"]["data_path"])
+    root_dir = os.getenv("OUTPUT_DIR", "./")
+    experiment = Experiment(root_dir=root_dir)
     experiment.from_config(config)
     experiment.prepare()
     experiment.train()
