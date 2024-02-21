71b8ab27d8b8c2140cbeaed15aa5721930c67035
pritchardn <21726929@student.uwa.edu.au>
2024-02-21 11:31:16 +0800

    Adjusts call to default paramters to include num_hidden variable.
    Changes exposure variable if available in environment.

--- src/main.py
+++ src/main.py
@@ -8,13 +8,16 @@ def main():
     model_type = os.getenv("MODEL_TYPE", "FC_LATENCY")
     dataset = os.getenv("DATASET", "HERA")
     num_hidden = int(os.getenv("NUM_HIDDEN", 128))
-    config = get_default_params(dataset, model_type)
+    config = get_default_params(dataset, model_type, num_hidden)
     config["data_source"]["data_path"] = os.getenv(
         "DATA_PATH", config["data_source"]["data_path"]
     )
     config["model"]["num_hidden"] = num_hidden
     config["data_source"]["limit"] = float(
         os.getenv("LIMIT", config["data_source"]["limit"])
+    )
+    config["encoder"]["exposure"] = int(
+        os.getenv("EXPOSURE", config["encoder"].get("exposure", None))
     )
     root_dir = os.getenv("OUTPUT_DIR", "./")
     experiment = Experiment(root_dir=root_dir)
