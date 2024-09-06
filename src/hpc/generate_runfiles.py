e3c8eeff591e7cae4f6618e1f5f2827f0bad26b6
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2024-09-06 11:10:19 +0800

    Makes repeat trials run on GPU partition.

--- src/hpc/generate_runfiles.py
+++ src/hpc/generate_runfiles.py
@@ -41,14 +41,12 @@ def prepare_singlerun(
 #SBATCH --job-name=SNN-SUPER-{model}-{encoding}-{dataset}
 #SBATCH --nodes={num_nodes}
 #SBATCH --time=24:00:00
-#SBATCH --mem={"230" if dataset == "LOFAR" else "115"}G 
-#SBATCH --cpus-per-task=32
-#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
 #SBATCH --output=super_%A_%a.out
 #SBATCH --error=super_%A_%a.err
 #SBATCH --array=0-9
-#SBATCH --partition=work
-#SBATCH --account=pawsey0411
+#SBATCH --partition=gpu
+#SBATCH --account=pawsey0411-gpu
 
 export DATASET="{dataset}"
 export LIMIT="1.0"
@@ -70,11 +68,11 @@ export OUTPUT_DIR="/scratch/pawsey0411/npritchard/outputs/snn-super/${{MODEL_TYP
 export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
 export MPICH_OFI_STARTUP_CONNECT=1
 export MPICH_OFI_VERBOSE=1
+export MPICH_GPU_SUPPORT_ENABLED=1
 export OMP_PLACES=cores     
 export OMP_PROC_BIND=close  
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
    
-srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS -m block:block:block python3 main.py
+srun -N 1 -n 1 -c 64 --gpus-per-task=8 --gpu-bind=closest python3 main.py
     """
     )
     return runfiletext
--- src/hpc/pawsey/RNN/LATENCY/HERA/DELTA_NORM/HERA-LATENCY.sh
+++ src/hpc/pawsey/RNN/LATENCY/HERA/DELTA_NORM/HERA-LATENCY.sh
@@ -2,14 +2,12 @@
 #SBATCH --job-name=SNN-SUPER-RNN_LATENCY-LATENCY-HERA
 #SBATCH --nodes=8
 #SBATCH --time=24:00:00
-#SBATCH --mem=115G 
-#SBATCH --cpus-per-task=32
-#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
 #SBATCH --output=super_%A_%a.out
 #SBATCH --error=super_%A_%a.err
 #SBATCH --array=0-9
-#SBATCH --partition=work
-#SBATCH --account=pawsey0411
+#SBATCH --partition=gpu
+#SBATCH --account=pawsey0411-gpu
 
 export DATASET="HERA"
 export LIMIT="1.0"
@@ -29,9 +27,9 @@ export OUTPUT_DIR="/scratch/pawsey0411/npritchard/outputs/snn-super/${MODEL_TYPE
 export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
 export MPICH_OFI_STARTUP_CONNECT=1
 export MPICH_OFI_VERBOSE=1
+export MPICH_GPU_SUPPORT_ENABLED=1
 export OMP_PLACES=cores     
 export OMP_PROC_BIND=close  
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
    
-srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS -m block:block:block python3 main.py
+srun -N 1 -n 1 -c 64 --gpus-per-task=8 --gpu-bind=closest python3 main.py
     
